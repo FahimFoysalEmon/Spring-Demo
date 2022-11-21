4591d0674de26238797e2777d4a4b038b4667d36
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2022-11-21 19:04:19 +0600

    constructor-args in beans

--- src/main/java/com/example/springdemo/Triangle.java
+++ src/main/java/com/example/springdemo/Triangle.java
@@ -4,16 +4,27 @@ public class Triangle {
 
     private String type;
 
+    private String message;
+
+    public Triangle(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public String getMessage() {
+        return message;
     }
 
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+
     public void draw() {
-        System.out.println(getType()+" is Drawn");
+        System.out.println(getType()+" is Drawn"+" "+getMessage());
     }
 
 }
