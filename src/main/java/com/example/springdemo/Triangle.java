cec7f7f928d98dd2c3c352938d258dc4d85cf4ce
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2022-11-22 14:37:38 +0600

    injecting objects

--- src/main/java/com/example/springdemo/Triangle.java
+++ src/main/java/com/example/springdemo/Triangle.java
@@ -2,29 +2,37 @@ package com.example.springdemo;
 
 public class Triangle {
 
-    private String type;
+   private Point pointA;
+   private Point pointB;
+   private Point pointC;
 
-    private String message;
-
-    public Triangle(String type, String message) {
-        this.type = type;
-        this.message = message;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public String getType() {
-        return type;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
     }
 
-    public String getMessage() {
-        return message;
+    public Point getPointB() {
+        return pointB;
     }
 
-//    public void setType(String type) {
-//        this.type = type;
-//    }
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
 
     public void draw() {
-        System.out.println(getType()+" is Drawn"+" "+getMessage());
+        System.out.println("Point A = {" + getPointA().getX() +", "+getPointA().getY() + ")");
+        System.out.println("Point B = {" + getPointB().getX() +", "+getPointB().getY() + ")");
+        System.out.println("Point C = {" + getPointC().getX() +", "+getPointC().getY() + ")");
     }
-
 }
